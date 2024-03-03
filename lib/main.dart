eea3b0469fe30139299f8e207384df218d40f761
Pormiiles <andrelima.santos2370@gmail.com>
2024-03-03 14:49:37 -0300

    Adicionando scrolling vertical na tela

--- lib/main.dart
+++ lib/main.dart
@@ -11,15 +11,17 @@ void main() {
           titleTextStyle: const TextStyle(color: Colors.white, fontSize: 32),
           toolbarHeight: 96,
         ),
-        body: const Column(
-          children: [
-            Center(
-              child: Image(image: AssetImage('imagens/90s-interface.jpg'),),
-            ),
-          Text('This Software Developer here is just chilling... ❤️'),
-          ],
+        body: const SingleChildScrollView(
+          child: Column(
+            children: [
+              Center(
+                child: Image(image: AssetImage('imagens/90s-interface.jpg'),),
+              ),
+            Text('This Software Developer here is just chilling... ❤️', style: TextStyle(color: Color.fromARGB(255, 236, 53, 217), fontSize: 20, fontWeight: FontWeight.bold)),
+            ],
+          ),
         ),
       ),
-    )
+    ),
   );
 }
